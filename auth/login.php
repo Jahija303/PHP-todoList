a54f105b7bfa24f3db75c1c1a7e10421a357eb4d
Jahija303 <57103143+Jahija303@users.noreply.github.com>
2021-05-07 16:56:00 +0200

    login and registration design

--- auth/login.php
+++ auth/login.php
@@ -4,6 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../css/login.css">
     <title>Login</title>
 </head>
 <body>
@@ -33,7 +39,6 @@ if(isset($_REQUEST['username'])) {
     if(mysqli_num_rows($retval) == 1) {
         
         $row = mysqli_fetch_assoc($retval);
-        session_start();
         $_SESSION['user_id'] = $row['user_id'];
         $_SESSION['username'] = $username;
 
@@ -48,29 +53,25 @@ if(isset($_REQUEST['username'])) {
 } else {
     ?>
 
-        <h2>Login</h2>
-      
-        <form method = "post" action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-            <table>
-                <tr>
-                    <td>Username:</td> 
-                    <td><input type = "text" name = "username"></td>
-                </tr>
-                
-                <tr>
-                    <td>Password:</td>
-                    <td><input type = "password" name = "password"></td>
-                </tr>
-
-                <tr>            
-                    <td><input type = "submit" name = "submit" value = "Submit"></td>                   
-                </tr>
-
-                <tr>
-                    <td><p><a href="/todoList/auth/registration.php">New Registration</a></p></td>
-                </tr>
-            </table>
-        </form>
+<div class="login-form">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+        <h2 class="text-center">Log in</h2>       
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Username" required="required" name = "username">
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" placeholder="Password" required="required" name = "password">
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary btn-block">Log in</button>
+        </div>
+        <div class="clearfix">
+            <label class="float-left form-check-label"><input type="checkbox"> Remember me</label>
+            <a href="#" class="float-right">Forgot Password?</a>
+        </div>        
+    </form>
+    <p class="text-center"><a href="/todoList/auth/registration.php">Create an Account</a></p>
+</div>
 
     <?php
 }
--- auth/registration.php
+++ auth/registration.php
@@ -4,6 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../css/login.css">
     <title>Register</title>
 </head>
 
@@ -54,10 +60,11 @@
             }
 
             if($result) {
-                echo "<div class='form'>
-                  <h3>You are registered successfully.</h3><br/>
-                  <p class='link'>Click here to <a href='/todoList/auth/login.php'>Login</a></p>
-                  </div>";
+                session_start();
+                $_SESSION['user_id'] = $row['user_id'];
+                $_SESSION['username'] = $username;
+                
+                header("Location: /todoList/index.php");
             } else {
                 echo "<div class='form'>
                       <h3>Required fields are missing.</h3><br/>
@@ -66,32 +73,30 @@
             }
          } else {
           ?>
-                <h2>Registration</h2>
-                
-                <form method = "post" action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-                    <table>
-                        <tr>
-                        <td>Username:</td> 
-                        <td><input type = "text" name = "username"></td>
-                        </tr>
-                        
-                        <tr>
-                        <td>Password:</td>
-                        <td><input type = "password" name = "password"></td>
-                        </tr>
-                        
-                        <tr>
-                        <td>E-Mail:</td>
-                        <td><input type = "text" name = "email"></td>
-                        </tr>
 
-                        <tr>
-                        <td>
-                            <input type = "submit" name = "submit" value = "Submit"> 
-                        </td>
-                        </tr>
-                    </table>
+            <div class="login-form">
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+                    <h2 class="text-center">Log in</h2>       
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Username" required="required" name = "username">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" placeholder="Password" required="required" name = "password">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" placeholder="E-mail" required="required" name = "email">
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary btn-block">Register</button>
+                    </div>
+                    <div class="clearfix">
+                        <label class="float-left form-check-label"><input type="checkbox"> Remember me</label>
+                        <a href="#" class="float-right">Forgot Password?</a>
+                    </div>        
                 </form>
+                <p class="text-center"><a href="/todoList/auth/login.php">Login</a></p>
+            </div>
+
              <?php
          }
         
