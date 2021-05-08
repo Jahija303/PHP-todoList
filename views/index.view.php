1d2c5412b3bc19c30e2b5779499707554e09f8d2
Jahija303 <57103143+Jahija303@users.noreply.github.com>
2021-05-08 05:45:19 +0200

    js update

--- views/index.view.php
+++ views/index.view.php
@@ -72,7 +72,34 @@
   
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
-<script src="/todoList/js/script.js"></script>
+
+<script>
+(function($) {
+    'use strict';
+    $(function() {
+
+        var todoListItem = $('.todo-list');
+
+        todoListItem.on('change', '.checkbox', function() {
+            if ($(this).attr('checked')) {
+                $(this).removeAttr('checked');
+                
+            } else {
+                $(this).attr('checked', 'checked');
+            }
+            
+            $(this).closest("li").toggleClass('completed');
+        
+        });
+        
+        todoListItem.on('click', '.remove', function() {
+            $(this).parent().remove();
+        });
+        
+    });
+
+})(jQuery);
+</script>
 
 </body>
 </html>
