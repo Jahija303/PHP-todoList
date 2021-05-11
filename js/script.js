2b65d83d069682ed64198dff09fced364b619a1f
Jahija303 <57103143+Jahija303@users.noreply.github.com>
2021-05-11 20:02:38 +0200

    removed code residue

--- js/script.js
+++ js/script.js
@@ -1,28 +1,3 @@
-/*(function($) {
-    'use strict';
-    $(function() {
-
-        var todoListItem = $('.todo-list');
-
-        todoListItem.on('change', '.checkbox', function() {
-            if ($(this).attr('checked')) {
-                $(this).removeAttr('checked');
-            } else {
-                $(this).attr('checked', 'checked');
-            }
-            
-            $(this).closest("li").toggleClass('completed');
-        
-        });
-        
-        todoListItem.on('click', '.remove', function() {
-            $(this).parent().remove();
-        });
-        
-    });
-
-})(jQuery);*/
-
 function toggleCheckboxDB(task_id) {
     let completed;
     var xmlhttp = new XMLHttpRequest();
